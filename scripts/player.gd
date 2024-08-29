b229a54af6eb2b4f9d3518cf99c4fd422c1b386c
SFloreaWSC <floreas@wsc.school.nz>
2024-08-29 23:53:48 +1200

    Improvements
    THE new AND improved sprite and movement stuff.

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,7 +12,7 @@ func _physics_process(_delta):
 	print(input_direction)
 	
 	if Input.get_action_strength("right") - Input.get_action_strength("left") == 1: 
-		animation.play("walkright")
+			animation.play("walkright")
 	elif Input.get_action_strength("right") - Input.get_action_strength("left") == -1:
 		animation.play("walkleft")
 	elif Input.get_action_strength("down") - Input.get_action_strength("up") == 1:
