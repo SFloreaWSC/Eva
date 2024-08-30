bf5348f3e21ed52e3cfe39a30424f190a8a709cd
SFloreaWSC <floreas@wsc.school.nz>
2024-08-31 00:33:44 +1200

    Minor player fixes

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,10 +9,9 @@ func _physics_process(_delta):
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 		)
-	print(input_direction)
-	
+	 
 	if Input.get_action_strength("right") - Input.get_action_strength("left") == 1: 
-			animation.play("walkright")
+		animation.play("walkright")
 	elif Input.get_action_strength("right") - Input.get_action_strength("left") == -1:
 		animation.play("walkleft")
 	elif Input.get_action_strength("down") - Input.get_action_strength("up") == 1:
